eb78f4f68da15f4e808130e76cb6bb010ef22179
Ostrovskaya Alexandra <59867572+LuvCrime@users.noreply.github.com>
2021-01-18 17:34:02 +0300

    refactoring

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,33 @@
 import './vendor';
 import './helpers';
+
+const MAX_VALUE = 100;
+const MY_VALUE = 90;
+
+let deg = 180 * MY_VALUE/MAX_VALUE;
+
+function rotateArrow() {
+    let temp = 0;
+
+    $('.counter').text(0);
+    $('.arrow').css({
+        transform: 'rotate(0deg)'
+    });
+
+    (function countSkill(temp) {
+        if(temp < MY_VALUE) {
+            setTimeout(() => {
+                countSkill(temp+=1)
+                $('.skill-result__counter').text(temp);
+            }, 800/MY_VALUE)
+        } else {
+            return;
+        }
+    })(temp);
+
+    $('.arrow').css({
+        transform: 'rotate(' + deg + 'deg)'
+    })
+}
+
+rotateArrow()
